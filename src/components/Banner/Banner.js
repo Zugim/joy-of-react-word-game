d329720800f0c1937289b2fe607347749a9983c2
Joe <joe.reeve@gmail.com>
2024-04-29 15:07:55 +0900

    Fixed formatting issue when displaying number of guesses it took to win

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,8 +6,11 @@ function Banner({ guessedCorrectly, currentNoOfGuesses, answer }) {
       <div className="happy banner">
         <p>
           <strong>Congratulations!</strong> Got it in{" "}
-          <strong>{currentNoOfGuesses} </strong>
-          {currentNoOfGuesses === 1 ? "guess." : "guesses."}
+          <strong>
+            {currentNoOfGuesses}{" "}
+            {currentNoOfGuesses === 1 ? "guess" : "guesses"}
+          </strong>
+          .
         </p>
       </div>
     );
